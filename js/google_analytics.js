4dc51301e65327a9a37ab0d3bf499a859559d989
Navaneeth <navaneeth.v@agribazaar.com>
2026-01-21 10:40:58 +0530

    google analytics added

---
+++ js/google_analytics.js
@@ -0,0 +1,8 @@
+if(window.location.host.includes('.in')){
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YJYGLGRQ2Z');
+}
+
+
